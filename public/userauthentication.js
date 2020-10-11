f4a3073cf8198389388dd1830cdfb1263ec9b3ce
Trenton Ward <trenton.w52@gmail.com>
2020-10-11 03:48:00 -0700

    Modified index.html to call OAuth services

--- public/userauthentication.js
+++ public/userauthentication.js
@@ -1,16 +1,5 @@
               // Initialize the FirebaseUI Widget using Firebase.
               var ui = new firebaseui.auth.AuthUI(firebase.auth());
-              ui.start('#firebaseui-auth-container', {
-              signInOptions: [
-              
-              // List of OAuth providers supported.
-              firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-              //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-              //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-              //firebase.auth.GithubAuthProvider.PROVIDER_ID
-              ],
-              // Other config options...
-              });
               
               //Creating instance of Google provider object
               var provider = new firebase.auth.GoogleAuthProvider();
@@ -37,3 +26,40 @@
                 var credential = error.credential;
                 // ...
               });
+
+              //FirebaseUI Configuration
+              var uiConfig = {
+                callbacks: {
+                  signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+                    // User successfully signed in.
+                    // Return type determines whether we continue the redirect automatically
+                    // or whether we leave that to developer to handle.
+                    return true;
+                  },
+                  uiShown: function() {
+                    // The widget is rendered.
+                    // Hide the loader.
+                    document.getElementById('loader').style.display = 'none';
+                  }
+                },
+                // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+                signInFlow: 'popup',
+                signInSuccessUrl: '<url-to-redirect-to-on-success>',
+                signInOptions: [
+                  // Leave the lines as is for the providers you want to offer your users.
+                  firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                  //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+                  //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+                  //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+                  //firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                  //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+                ],
+                // Terms of service url.
+                tosUrl: '<your-tos-url>',
+                // Privacy policy url.
+                privacyPolicyUrl: '<your-privacy-policy-url>'
+              };
+
+              //Start Firebase UI Auth Interface
+              // The start method will wait until the DOM is loaded.
+              ui.start('#firebaseui-auth-container', uiConfig);
