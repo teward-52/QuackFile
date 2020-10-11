055bfd768237f6f52af809fe34471c39a3157f60
Trenton Ward <trenton.w52@gmail.com>
2020-10-10 17:29:40 -0700

    New index.html and beginning of app.js file, reorganized Public directory

---
+++ public/app.js
@@ -0,0 +1,15 @@
+            // Your web app's Firebase configuration
+            // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+            var firebaseConfig = {
+                apiKey: "AIzaSyAcHoj4SzNJo9-ldyLMRA9HfFoVhWG6loA",
+                authDomain: "quackfile.firebaseapp.com",
+                databaseURL: "https://quackfile.firebaseio.com",
+                projectId: "quackfile",
+                storageBucket: "quackfile.appspot.com",
+                messagingSenderId: "140319055728",
+                appId: "1:140319055728:web:c5606d9035e4f07e2af9b5",
+                measurementId: "G-HL35W51LBF"
+                };
+                // Initialize Firebase
+                firebase.initializeApp(firebaseConfig);
+                firebase.analytics();
