3acc44d4c3ec2c078745c3ebe23168c5dc1800f7
Yoni Rosenbloom <yonirosenbloom@hotmail.com>
2020-10-11 00:49:33 -0700

    Account object file created

---
+++ Account.js
@@ -0,0 +1,45 @@
+console.log("CONNECTED");
+
+function Account(username, password, fileObj) {
+    this.username = username;
+    this.password = password;
+    this.fileObj = fileObj;
+}
+
+let un = {
+    get username() {
+
+
+    },
+
+    set username(username) {
+        
+
+    }
+};
+
+let pw = {
+    get password() {
+
+
+    },
+
+    set password(password) {
+
+
+    }
+};
+
+let fo = {
+    get fileObj() {
+
+
+    },
+
+    set fileObj(fileObj) {
+
+
+    }
+
+
+};
